88e6bd1bf4a2d24cf56da5329d93de6842e90bd4
David Thompson <mail@fatbusinessman.com>
2016-02-07 16:57:38 +0000

    Download the fixtures data
    This dumbly requests the URL every time, but might need some caching
    love, depending on how Google decides to behave. It might be sufficient
    just to propagate the original request's cache headers.

--- fixtures.py
+++ fixtures.py
@@ -1,12 +1,18 @@
 from sys import stdin
+
 from icalendar import Calendar
+import requests
+
+
+FIXTURES_URL = 'http://www.cambridge-united.co.uk/generic/download-fixtures-calendar.aspx?teamID=71'
 
 
 def is_home_game(fixture):
     return fixture['location'] == 'Abbey Stadium'
 
+response = requests.get(FIXTURES_URL)
 
-fixtures = Calendar.from_ical(stdin.read())
+fixtures = Calendar.from_ical(response.text)
 fixtures.subcomponents = filter(is_home_game, fixtures.subcomponents)
 
 print fixtures.to_ical()
